5ca96f360c44f3f42da15c73523f964000f492f8
ameyawagh <ameya555@gmail.com>
2018-01-04 13:29:58 -0500

    added about me from javascript

---
+++ assets/js/about_me.js
@@ -0,0 +1,8 @@
+var about_me = 
+["I am a highly passion driven engineer currently pursuing Ms in Robotics at WPI.",
+" I have a Bachelors degree in Electronics and Telecommunication from University of Mumbai ",
+"and have been constantly associated with robotics competitions like ABU Robocon and Eyantra for most of my undergraduate term.",
+" I am skilled at embedded systems and robotics with a strong hold on Python, C, C++ and Matlab. ",
+"My research interests include Robot perception and Artificial Intelligence."].join("");
+
+$("#about_me").text(about_me);
---
+++ assets/js/console_text.js
@@ -0,0 +1,4 @@
+var console_txt = "hello"
+console.log("rendering console_txt")
+
+$("#console_txt").text(console_txt)
